8f589dd1ada8a7a24aafa16468259bc235983686
alex <45722053p@iespoblenou.org>
2016-10-20 17:07:51 +0200

    Boton de refresh hecho en el fragment.

--- app/src/main/java/com/proyecto/listmagiccards/MainActivityFragment.java
+++ app/src/main/java/com/proyecto/listmagiccards/MainActivityFragment.java
@@ -1,8 +1,12 @@
 package com.proyecto.listmagiccards;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -22,6 +26,13 @@ public class MainActivityFragment extends Fragment {
 
     public MainActivityFragment() {
 
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setHasOptionsMenu(true);
     }
 
     @Override
@@ -55,4 +66,30 @@ public class MainActivityFragment extends Fragment {
 
         return FragmentView;
     }
+    //Estos metodos son para las opciones del menu del fragment.
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.menu_cartas_refresh, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.refresh) {
+            refresh();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    //En este metodo colocaremos la accion que hara el refresh que sera hacer una llamada a la api en segundo plano.
+    private void refresh() {
+    }
+
 }
