c261acc16893feec128fee9a8ac9b1a947eca981
alex <45722053p@iespoblenou.org>
2016-10-31 16:15:41 +0100

    layout del details hecho, con todos los texts view que queramos ver en la carta que abramos.

--- app/src/main/java/com/proyecto/listmagiccards/DetailsActivityFragment.java
+++ app/src/main/java/com/proyecto/listmagiccards/DetailsActivityFragment.java
@@ -1,7 +1,9 @@
 package com.proyecto.listmagiccards;
 
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,6 +19,28 @@ public class DetailsActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_details, container, false);
+        View detailsFragment = inflater.inflate(R.layout.fragment_details, container, false);
+
+        Intent i = getActivity().getIntent();
+
+        if(i != null){
+             Cards carta = (Cards)i.getSerializableExtra("carta");
+
+            if(carta != null){
+
+                updateUi(carta);
+
+            }
+
+        }
+
+        return detailsFragment;
+
     }
+
+    private void updateUi(Cards carta){
+
+        Log.d("CARTA",carta.toString());
+    }
+
 }
