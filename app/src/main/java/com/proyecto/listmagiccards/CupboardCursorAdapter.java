6824372f0dac89ba85b80ebf651c3c97c95c07d1
alex <45722053p@iespoblenou.org>
2016-11-11 14:15:27 +0100

    CursorAdapter acabado siguiente paso hacer el adapter cursor

--- app/src/main/java/com/proyecto/listmagiccards/CupboardCursorAdapter.java
+++ app/src/main/java/com/proyecto/listmagiccards/CupboardCursorAdapter.java
@@ -8,35 +8,34 @@ import android.widget.CursorAdapter;
 
 import nl.qbusict.cupboard.Cupboard;
 
-import static android.icu.lang.UCharacter.GraphemeClusterBreak.T;
 import static nl.qbusict.cupboard.CupboardFactory.cupboard;
 
 /**
  * Created by 45722053p on 08/11/16.
  */
 
-public class CupboardCursorAdapter  extends CursorAdapter{
+public abstract class CupboardCursorAdapter <Cards> extends CursorAdapter{
 
     private final Cupboard mCupboard;
-    private Class<T> mEntityClass;
+    private Class<Cards> mEntityClass;
 
-    public CupboardCursorAdapter(Context context, Class<T> entityClass) {
+    public CupboardCursorAdapter(Context context, Class<Cards> entityClass) {
         this(context, cupboard(), entityClass, null);
     }
 
-    public CupboardCursorAdapter(Context context, Cupboard cupboard, Class<T> entityClass) {
+    public CupboardCursorAdapter(Context context, Cupboard cupboard, Class<Cards> entityClass) {
         this(context, cupboard, entityClass, null);
     }
 
-    public CupboardCursorAdapter(Context context, Cupboard cupboard, Class<T> entityClass, Cursor cursor) {
+    public CupboardCursorAdapter(Context context, Cupboard cupboard, Class<Cards> entityClass, Cursor cursor) {
         super(context, cursor, false);
         this.mEntityClass = entityClass;
         this.mCupboard = cupboard;
     }
 
-    public abstract View newView(Context context, T model, ViewGroup parent);
+    public abstract View newView(Context context, Cards model, ViewGroup parent);
 
-    public abstract void bindView(View view, Context context, T model);
+    public abstract void bindView(View view, Context context, Cards model);
 
     @Override
     public View newView(Context context, Cursor cursor, ViewGroup parent) {
@@ -48,7 +47,7 @@ public class CupboardCursorAdapter  extends CursorAdapter{
         bindView(view, context, getItem(cursor.getPosition()));
     }
 
-    public T getItem(int position) {
+    public Cards getItem(int position) {
         if (getCursor().moveToPosition(position)) {
             return mCupboard.withCursor(getCursor()).get(mEntityClass);
         } else {
